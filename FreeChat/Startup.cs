d6fc12e64c5604d5f6e418cd6165f1418b43c2b0
kkblues <kkkapasakis@yahoo.gr>
2018-01-03 12:00:25 +0200

    Initial Commit
    Project Creation

---
+++ FreeChat/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(FreeChat.Startup))]
+namespace FreeChat
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
