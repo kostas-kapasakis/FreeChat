a8b909cae7b96b595a3210db1f8d7449c0e89eea
kkblues <kkkapasakis@yahoo.gr>
2018-01-20 12:14:17 +0200

    Add Intial Seeding Migration For Topics Table

---
+++ FreeChat/Migrations/201801201009413_ProperSeedingToTopicsTable.cs
@@ -0,0 +1,49 @@
+namespace FreeChat.Migrations
+{
+    using System.Data.Entity.Migrations;
+
+    public partial class ProperSeedingToTopicsTable : DbMigration
+    {
+        public override void Up()
+        {
+
+
+            Sql(@"INSERT INTO [dbo].[Topics] ([Name],[Genre],[Active],[DateCreated],[DateExpired],[MaxClientsOnline],[Description]) VALUES (N'Led Zeppelin', N'Music',1,20/1/2018,20/2/2018,100, N'Is Jimmy Page the reason why Lez Zeppelin were great ? ')");
+
+            Sql(@"INSERT INTO [dbo].[Topics] ([Name],[Genre],[Active],[DateCreated],[DateExpired],[MaxClientsOnline],[Description]) VALUES (N'Beatles', N'Music',1,20/1/2018,20/2/2018,100, N'How good exactly beatles were ?')");
+
+            Sql(@"INSERT INTO [dbo].[Topics] ([Name],[Genre],[Active],[DateCreated],[DateExpired],[MaxClientsOnline],[Description]) VALUES (N'Trypes', N'Music',1,20/1/2018,20/2/2018,100, N'Best Greek Rock Band  ? ')");
+
+            Sql(@"INSERT INTO [dbo].[Topics] ([Name],[Genre],[Active],[DateCreated],[DateExpired],[MaxClientsOnline],[Description]) VALUES (N'Mainstream', N'Music',1,20/1/2018,20/2/2018,100, N'In how may ways mainstream music passes the wrond message to youth ? ')");
+
+            Sql(@"INSERT INTO [dbo].[Topics] ([Name],[Genre],[Active],[DateCreated],[DateExpired],[MaxClientsOnline],[Description]) VALUES (N'Panousis Death', N'Music',1,20/1/2018,20/2/2018,100, N'Our jim Panousis death ,chat about his life')");
+
+
+
+            Sql(@"INSERT INTO [dbo].[Topics] ([Name],[Genre],[Active],[DateCreated],[DateExpired],[MaxClientsOnline],[Description]) VALUES (N'Nba All star game', N'Sports',1,20/1/2018,20/2/2018,100, N'Giannis again in all star game')");
+
+            Sql(@"INSERT INTO [dbo].[Topics] ([Name],[Genre],[Active],[DateCreated],[DateExpired],[MaxClientsOnline],[Description]) VALUES (N'Champions League', N'Sports',1,20/1/2018,20/2/2018,100,N'What happen to Barcelona ? ')");
+
+            Sql(@"INSERT INTO [dbo].[Topics] ([Name],[Genre],[Active],[DateCreated],[DateExpired],[MaxClientsOnline],[Description]) VALUES (N'VoleyBall Finals', N'Sports',1,20/1/2018,20/2/2018,100,N'What do you predict ? ')");
+
+            Sql(@"INSERT INTO [dbo].[Topics] ([Name],[Genre],[Active],[DateCreated],[DateExpired],[MaxClientsOnline],[Description]) VALUES (N'Sports Violence', N'Sports',1,20/1/2018,20/2/2018,100,N'How to stop,react,avoid sports violence ? ')");
+
+            Sql(@"INSERT INTO [dbo].[Topics] ([Name],[Genre],[Active],[DateCreated],[DateExpired],[MaxClientsOnline],[Description]) VALUES (N'Team Owners', N'Sports',1,20/1/2018,20/2/2018,100,N'Are all of them some kind og gangsters ? ')");
+
+
+            Sql(@"INSERT INTO [dbo].[Topics] ([Name],[Genre],[Active],[DateCreated],[DateExpired],[MaxClientsOnline],[Description]) VALUES (N'Europe', N'Trips',1,20/1/2018,20/2/2018,100,N'Best places to visit in Europe ')");
+
+            Sql(@"INSERT INTO [dbo].[Topics] ([Name],[Genre],[Active],[DateCreated],[DateExpired],[MaxClientsOnline],[Description]) VALUES (N'Sites for trips Discounts', N'Trips',1,20/1/2018,20/2/2018,100,N'List of any site offers discounts for trips ? ')");
+
+            Sql(@"INSERT INTO [dbo].[Topics] ([Name],[Genre],[Active],[DateCreated],[DateExpired],[MaxClientsOnline],[Description]) VALUES (N'AOP', N'Trips',1,20/1/2018,20/2/2018,100,N'Hiking team of peiraias organizes hikes in greece mountains ? ')");
+
+            Sql(@"INSERT INTO [dbo].[Topics] ([Name],[Genre],[Active],[DateCreated],[DateExpired],[MaxClientsOnline],[Description]) VALUES (N'England', N'Trips',1,20/1/2018,20/2/2018,100,N'Best city to visit except London ? ')");
+
+            Sql(@"INSERT INTO [dbo].[Topics] ([Name],[Genre],[Active],[DateCreated],[DateExpired],[MaxClientsOnline],[Description]) VALUES (N'Pacific Ocean', N'Trips',1,20/1/2018,20/2/2018,100,N'Any info about pacifics ocean islands ? ')");
+        }
+
+        public override void Down()
+        {
+        }
+    }
+}
