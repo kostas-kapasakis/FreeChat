72d8f640571fb742ae9c647727e338b720bd78d9
kkblues <kkkapasakis@yahoo.gr>
2018-01-29 23:57:31 +0200

    Create Chat engine controller and view

---
+++ FreeChat/Controllers/ChatEngineController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FreeChat.Controllers
+{
+    public class ChatEngineController : Controller
+    {
+        
+        public ActionResult Chatengine()
+        {
+            return View("Chatengine");
+        }
+    }
+}
