79081c92bae47693c1c87867f33845e332441604
Kostas Kapasakis <kkkapasakis@yahoo.gr>
2018-04-11 18:55:47 +0300

    Arranging The TopicsDto

--- FreeChat.Core/Models/DTO/TopicsFullDto.cs
+++ FreeChat.Core/Models/DTO/TopicsFullDto.cs
@@ -1,6 +1,4 @@
 ﻿using System;
-using System.ComponentModel;
-using System.ComponentModel.DataAnnotations;
 
 namespace FreeChat.Core.Models.DTO
 {
@@ -8,21 +6,22 @@ namespace FreeChat.Core.Models.DTO
     {
         public long Id { get; set; }
 
-        [Required]
-        [StringLength(50)]
-        [DisplayName("Room Name")]
         public string Name { get; set; }
 
         public string Genre { get; set; }
 
-        [StringLength(250)]
-        [Required]
         public string Description { get; set; }
+
         public bool Active { get; set; }
+
         public DateTime DateCreated { get; set; }
+
         public DateTime DateExpired { get; set; }
+
         public long MaxClientsOnline { get; set; }
+
         public string UserCreatorId { get; set; }
+
         public long MainCategoryId { get; set; }
     }
 }
